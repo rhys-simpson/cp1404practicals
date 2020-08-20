bb46d6a216b82650d39397793cc75d76d5906d4b
rhys-simpson <rhys030102@gmail.com>
2020-08-20 16:22:59 +1000

    Prac 3 broken score, (can change to fix variable name error)

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,12 +5,30 @@ Program to determine score status
 Rhys Simpson
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+
+def main():
+    # Get user score and print corresponding grade
+    score = float(input("Enter score: "))
+    print(determine_grade(score))
+
+
+# Determine grade
+def determine_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+# c. wasn't sure if we were suppose to add another function for random score
+score = random.uniform(1, 100)
+print("Random score was: {:,.2f}".format(score))
+print(determine_grade(score))
+
+main()
