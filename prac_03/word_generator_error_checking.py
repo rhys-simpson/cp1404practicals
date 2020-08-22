e3c091947186ca45fa4b7724c980fc41280bc559
rhys-simpson <rhys030102@gmail.com>
2020-08-22 18:28:26 +1000

    prac 3 - word generator error checking

---
+++ prac_03/word_generator_error_checking.py
@@ -0,0 +1,18 @@
+"""
+CP1404
+Random word generator
+"""
+import random
+
+VOWELS = "aeiou"
+CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+
+word_format = "ccvcvvc"
+word = ""
+for kind in word_format:
+    if kind == "c":
+        word += random.choice(CONSONANTS)
+    else:
+        word += random.choice(VOWELS)
+
+print(word)
