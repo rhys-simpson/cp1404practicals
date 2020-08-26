f46f6dd5b408388a86e8601842b6430d57e4ebe1
rhys-simpson <rhys030102@gmail.com>
2020-08-26 12:51:22 +1000

    completed

--- prac_02/randoms.py
+++ prac_02/randoms.py
@@ -4,6 +4,15 @@ Code that produces a random number between 1 and 100 inclusive
 
 Rhys Simpson
 """
+# 1.
+# smallest number 5; largest number 20
+
+# 2.
+# smallest number 3; largest number 9
+# no it can only produce 3, 5, 7, 9
+
+# 3.
+# smallest number 2.5000000000000000; largest number 5.5000000000000000
 
 import random
 print(random.randint(1, 100))
