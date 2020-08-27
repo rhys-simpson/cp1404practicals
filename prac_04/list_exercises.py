07d196fd673d9fd1d11856124c1472cadc236fc4
rhys-simpson <rhys030102@gmail.com>
2020-08-27 12:56:17 +1000

    prac 4 - completed

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,6 +5,7 @@ List practice
 Rhys Simpson
 """
 
+# Basic List operations
 numbers = []
 
 for i in range(5):
@@ -16,3 +17,13 @@ print("The last number is {}.".format(numbers[-1]))
 print("The smallest number is {}.".format(min(numbers)))
 print("The largest number is {}.".format(max(numbers)))
 print("The average of the numbers is {}.".format(sum(numbers)/len(numbers)))
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter a user name: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
