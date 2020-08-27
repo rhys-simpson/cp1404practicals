a6cfbe661bd78f55eda81dbe6f0416cd7f5ea9bd
rhys-simpson <rhys030102@gmail.com>
2020-08-27 12:21:03 +1000

    prac 4 - total income completed

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -19,6 +19,7 @@ def main():
 
 
 def print_report(incomes, number_of_months):
+    """ print income report """
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
