be15a7ea74298efebe9813f1d905bad67c8e362d
rhys-simpson <rhys030102@gmail.com>
2020-08-27 14:15:09 +1000

    done

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -15,17 +15,16 @@ def main():
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
     """ print income report """
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month+1, income, total))
 
 
 main()
