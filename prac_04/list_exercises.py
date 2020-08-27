f70fc51b6ba10b550edd08335f4ebfd5f3b3e24a
rhys-simpson <rhys030102@gmail.com>
2020-08-27 12:50:24 +1000

    prac 4 - basic list operations completed

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical
+List practice
+
+Rhys Simpson
+"""
+
+numbers = []
+
+for i in range(5):
+    number = int(input("Enter number {}: ".format(i+1)))
+    numbers.append(number)
+
+print("The first number is {}.".format(numbers[0]))
+print("The last number is {}.".format(numbers[-1]))
+print("The smallest number is {}.".format(min(numbers)))
+print("The largest number is {}.".format(max(numbers)))
+print("The average of the numbers is {}.".format(sum(numbers)/len(numbers)))
