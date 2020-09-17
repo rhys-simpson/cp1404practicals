95dfb851f81b432492428cc6b3cf88302cf9b260
rhys-simpson <rhys030102@gmail.com>
2020-09-17 13:46:11 +1000

    demos

---
+++ prac_07/KivyDemos-master/convert_m_to_km_solution.py
@@ -0,0 +1,44 @@
+"""
+CP1404 Week 11 Workshop - GUI program to convert miles to kilometres
+Lindsay Ward, IT@JCU
+06/10/2015
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+__author__ = 'Lindsay Ward'
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_m_km_solution.kv')
+        return self.root
+
+    def handle_calculate(self):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, change):
+        """Handle up/down button press, update the text input with new value, call calculation function."""
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+
+    def get_validated_miles(self):
+        """Convert text to float or 0.0 if invalid."""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+
+MilesConverterApp().run()
