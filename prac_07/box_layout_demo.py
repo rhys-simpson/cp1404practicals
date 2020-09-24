5d445c703473b4e7efb09714c428a40e231d3fba
rhys-simpson <rhys030102@gmail.com>
2020-09-24 13:36:55 +1000

    prac 7

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -9,15 +9,20 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Program demoing buttons"""
+
     def build(self):
+        """Build the Kivy app from the kv file."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle output label text"""
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
     def handle_clear(self):
+        """Handle clearing input box"""
         self.root.ids.output_label.text = "Enter your name"
         self.root.ids.input_name.text = ""
 
--- prac_07/dynamic_widgets.py
+++ prac_07/dynamic_widgets.py
@@ -13,17 +13,17 @@ class DynamicWidgetsApp(App):
     """Program demoing dynamic widgets"""
 
     def build(self):
-        """ """
+        """Build the Kivy app from the kv file."""
         self.title = "Dynamic Widgets"
         self.root = Builder.load_file('dynamic_widgets.kv')
         self.create_widgets()
         return self.root
 
     def create_widgets(self):
-        """ """
+        """Create labels from list and add them to GUI"""
         names = ["Jim Bob", "Jeff Bob", "Jimmy Bob"]
         for name in names:
-            label = Label(text=name, id=name)
+            label = Label(text=name)
             self.root.ids.entries_box.add_widget(label)
 
 
